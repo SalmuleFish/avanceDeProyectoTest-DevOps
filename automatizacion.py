0153f23f86a0ce9d4266f272f571e9e7af0a90e1
SalmuleFish <al03026542@tecmilenio.mx>
2026-04-08 17:56:35 -0600

    refactor: rename project resources and configuration from STF to STP

--- automatizacion.py
+++ automatizacion.py
@@ -5,12 +5,11 @@ def generar_y_subir_reporte():
     ec2 = boto3.client('ec2', region_name='us-east-1')
     s3 = boto3.client('s3', region_name='us-east-1')
     
-    # Nombre de tu bucket (Cambiá el número por el tuyo si es distinto)
-    nombre_bucket = "reportes-stf-590184134445"
+    nombre_bucket = f"reportes-stp-{AWS::AccountId}"
     nombre_archivo = f"reporte-{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}.txt"
     
     # Generar el contenido del reporte
-    contenido = "=== REPORTE DE RECURSOS STF ===\n\n"
+    contenido = "=== REPORTE DE RECURSOS STP ===\n\n"
     
     instances = ec2.describe_instances()
     contenido += "--- Instancias EC2 ---\n"
@@ -22,7 +21,7 @@ def generar_y_subir_reporte():
     with open(nombre_archivo, "w") as f:
         f.write(contenido)
     
-    # SUBIR AL S3 🚀
+    # SUBIR AL S3
     print(f"Subiendo {nombre_archivo} al bucket {nombre_bucket}...")
     s3.upload_file(nombre_archivo, nombre_bucket, nombre_archivo)
     print("¡Reporte guardado en S3 con éxito!")
